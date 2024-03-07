d57b9bb137e4b581af449d082f0055440f910621
20221023lijia <fahuihaovvguanyu@gmail.com>
2024-03-07 21:12:48 +0400

    7

--- plot2s.m
+++ plot2s.m
@@ -2,5 +2,6 @@ function plot2s(frame_data,y_data, figure_number,  y_label)
    figure(figure_number);
    plot(frame_data, y_data); 
    ylabel(y_label); 
-   xlabel('$\Phi \ \degree$','Interpreter','Latex'); 
+   xlabel('\Phi^\circ'); 
+   grid on
 end
