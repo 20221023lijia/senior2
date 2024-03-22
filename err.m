7f85671845a83bb9a84fb776599c6f5aca19db2a
20221023lijia <fahuihaovvguanyu@gmail.com>
2024-03-22 23:13:24 +0400

    1

--- err.m
+++ err.m
@@ -1,4 +1,5 @@
 function err(X_bottom,N,x)
+%if 理论值 实验值 {除的实验值，计算的实验误差}
 err=abs(X_bottom-N)/abs(N);
 fprintf('%s =%.6f\n',x,err)
 end
--- shell1.m
+++ shell1.m
@@ -1,6 +1,8 @@
 function [epsilon_1,sigma_i,count,sigma_kp,tau_KP_1]=shell1(e,sigma_p,k0,kp,km,delta_Shell,R,sigma2,ee,sigma_1_)
 E = 6.8 * 10^4;%MPa
-a = 1.4;d = 0.032;b = 0.146;c = 0.0108;ge = 0.779;
+A= 1.2406;B = 0.095603;D= 0.059312;G = 0.6812;
+a=A;b=B;c=0;d=D;ge=G;
+%程序中输入的是ABDC
 sigma_i(1)=e;sigma_kp(1)=ee;
 tau_KP_1(1)=ee;
 i=1;    epsilon_1(1) = sigma_i(1)/ sigma_p;
@@ -14,6 +16,8 @@ while(sigma_i(i)>sigma_p)
     while(epsilon_1(i)>1)
         E_k(i+1) = E * ( (b / (epsilon_1(i) - ge)^2) - (2*c / (epsilon_1(i) - ge)^3) + d );
         E_c(i+1) = (E / epsilon_1(i)) * ( (a - b/(epsilon_1(i) - ge) - c/(epsilon_1(i) - ge)^2 + d * epsilon_1(i)) );
+%         E_k(i+1) = E * ( B/(epsilon_1(i) - G)^2 + D );
+%         E_c(i+1) = (E / epsilon_1(i)) * (A- B/(epsilon_1(i) - G) + D * epsilon_1(i));
         i=i+1;
         ki(i)=sqrt(E_k(i)* E_c(i))/E;
         k1(i)= k0*kp*km*ki(i);
